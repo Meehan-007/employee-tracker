8bca9093edc402f5f15638e0b1bbfb8601619d6e
Meehan-007 <93944358+Meehan-007@users.noreply.github.com>
2023-05-20 16:46:19 -0400

    updated the index menu to have exit and fixed some bugs

--- index.js
+++ index.js
@@ -11,51 +11,63 @@ import inquirer from 'inquirer';
             choices: ['view all departments', 'view all roles', 
             'view all employees', 
             'add a department', 'add a role', 
-            'add an employee', 'update an employee']
+            'add an employee', 'update an employee', 'exit']
           }
         ]).then((answers) => {
 
-    if (answers === 'view all deperatments') { 
-        console.log(answers)
+    if (answers.item === 'view all departments') { 
+        
         console.log('viewing departments')
         viewDepartments();
     } 
 
-    if (answers === 'view all roles') { 
-        console.log(answers)
+    if (answers.item === 'view all roles') { 
+        
         console.log('viewing roles')
         viewRoles();
     }  
 
-    if (answers === 'view all employees') { 
-        console.log(answers)
+    if (answers.item === 'view all employees') { 
+       
         console.log('viewing employees')
         viewEmployees();
     }  
 
-    if (answers === 'add a department') { 
-        console.log(answers)
+    if (answers.item === 'add a department') { 
+       
         console.log('add departments')
         addDepartments();
     }  
 
-    if (answers === 'add a role') { 
-        console.log(answers)
+    if (answers.item === 'add a role') { 
+        
         console.log('add roles')
         addRoles();
     }  
 
-    if (answers === 'add an employee') { 
-        console.log(answers)
+    if (answers.item === 'add an employee') { 
+        
         console.log('add employees')
         addEmployees();
     }  
 
-    if (answers === 'update an employee') { 
-        console.log(answers)
+    if (answers.item === 'update an employee') { 
+        
         console.log('update Employees')
         updateEmployees();
     }  
+
+    if (answers.item === 'exit') { 
+        
+        console.log('bye')
+        process.exit(); 
+    }  
+
+    // else {
+    //     console.log("syntax error") 
+    //     console.log(answers.item); 
+    //     init();
+    // }
 
    
     })
@@ -67,55 +79,64 @@ import inquirer from 'inquirer';
 
 
 
-function viewDepartments(){
+function viewDepartments(){ 
+    // WHEN I choose to view all departments
+// THEN I am presented with a formatted table showing department names and department ids
     console.log("now viewing departments") 
     init();
 } 
 
-function viewRoles(){
+function viewRoles(){ 
+    // WHEN I choose to view all roles
+// THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
     console.log("now viewing roles") 
     init();
 } 
 
-function viewEmployees(){
+function viewEmployees(){ 
+    // WHEN I choose to view all employees
+// THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
     console.log("now viewing employees") 
     init();
 } 
 
-function addEmployees(){
+function addEmployees(){ 
+ 
+
+// WHEN I choose to add an employee
+// THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
     console.log("add employees") 
     init();
 } 
 
-function addRoles(){
+function addRoles(){ 
+
+// WHEN I choose to add a role
+// THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
     console.log("add roles") 
     init();
 } 
 
-function addDepartments(){
+function addDepartments(){ 
+       // WHEN I choose to add a department
+// THEN I am prompted to enter the name of the department and that department is added to the database 
     console.log("add department") 
     init();
 } 
 
-function updateEmployees(){
+function updateEmployees(){ 
+
+// WHEN I choose to update an employee role
+// THEN I am prompted to select an employee to update and their new role and this information is updated in the database 
+
     console.log("update employees") 
     init();
 }
 
-// WHEN I choose to view all departments
-// THEN I am presented with a formatted table showing department names and department ids
-// WHEN I choose to view all roles
-// THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
-// WHEN I choose to view all employees
-// THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
-// WHEN I choose to add a department
-// THEN I am prompted to enter the name of the department and that department is added to the database
-// WHEN I choose to add a role
-// THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
-// WHEN I choose to add an employee
-// THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
-// WHEN I choose to update an employee role
-// THEN I am prompted to select an employee to update and their new role and this information is updated in the database 
+
+
+
+
 
 
 
